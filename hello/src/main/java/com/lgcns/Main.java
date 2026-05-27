99d55a505e2b1181b7cca6f0126f6e16ded0b052
indiflex <indiflex1@gmail.com>
2026-05-27 14:49:46 +0900

    primitive

--- hello/src/main/java/com/lgcns/Main.java
+++ hello/src/main/java/com/lgcns/Main.java
@@ -6,15 +6,21 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello %s%n", "Jadexx");
+        System.out.printf("Hello %s, %03d세, %.2fcm%n", "Jadexx", 33, 179.5);
         System.out.println("Jade!!");
+//        String name = new String("Jade");
+//        String nick = new String("Jade");
         String name = "Jade";
+        String nick = "Jade";
         System.out.println("name = " + name);
+        System.out.println("name==nick:" + (name == nick) + ", " + name.equals(nick));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("%f%n", 0.1 + 0.2);
+        System.out.println(0.1 + 0.2);
+
+        float f = 0.99999998f;
+        double d = 0.99999998;
+        System.out.println("f, d = " + f + ", " + d);
+
     }
 }
