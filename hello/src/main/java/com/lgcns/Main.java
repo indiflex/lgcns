f916cf647a7068d27d9f81409be8512517928032
indiflex <indiflex1@gmail.com>
2026-05-27 14:03:39 +0900

    sample

--- hello/src/main/java/com/lgcns/Main.java
+++ hello/src/main/java/com/lgcns/Main.java
@@ -7,6 +7,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello %s%n", "Jadexx");
+        System.out.println("Jade!!");
+        String name = "Jade";
+        System.out.println("name = " + name);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
